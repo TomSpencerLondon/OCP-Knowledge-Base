aa28172a4a969865ea6350edb5b819638806d027
Tom <thomas.spencer@codurance.com>
2021-06-08 20:09:22 +0100

    Add executor.shutdown() for Callable

--- src/main/java/com/codurance/FirstClass.java
+++ src/main/java/com/codurance/FirstClass.java
@@ -1,24 +1,39 @@
 package com.codurance;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import javax.print.DocFlavor.STRING;
+
 public class FirstClass {
   // Java program - start a process
   // Java can run 100s of threads
   // JVM - designates one thread and runs code
   // in the main method
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
 
     // This thread takes time to spawn
-    Runnable runnable = new Runnable() {
+    Thread thread = new Thread(() -> System.out.println("Hello from runnable 1"));
+    thread.start();
+
+    Callable<String> callable = new Callable() {
       @Override
-      public void run() {
-        System.out.println("Hello from runnable 1");
+      public String call() throws Exception {
+        return "Completed call";
       }
     };
 
-    Thread thread = new Thread(runnable);
-    thread.start();
+    ExecutorService executor = Executors.newFixedThreadPool(2);
+    Future<String> submitted = executor.submit(callable);
+
+    System.out.println(submitted.get());
+
 
     System.out.println("Hello world!");
+
+    executor.shutdown();
   }
 }
